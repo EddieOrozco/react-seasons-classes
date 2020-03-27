4042d8565925d0d2c6bb0adf9e9a08310d5c2384
EddieOrozco <orozcoe230@gmail.com>
2020-03-27 14:25:07 -0500

    Refacorimg, Using Life cycle methods

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => {
         this.setState({ lat: position.coords.latitude });
@@ -19,6 +21,7 @@ class App extends React.Component {
       }
     );
   }
+
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
