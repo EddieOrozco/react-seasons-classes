110901d1874ed6a584eaaae26c0fa1fd6ee4bb10
EddieOrozco <orozcoe230@gmail.com>
2020-03-27 13:07:00 -0500

    How to change state with setState and render to the page

--- src/index.js
+++ src/index.js
@@ -7,13 +7,16 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null };
-  }
-  render() {
+
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
       err => console.log(err)
     );
-    return <div>Latitude: </div>;
+  }
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
